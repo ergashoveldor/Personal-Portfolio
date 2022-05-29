492dd6a09eadc571b18f6fec79dc7801eb99d3f4
ergashoveldor <eldor7147@gmail.com>
2022-05-29 08:04:47 +0500

    desktop version finished

---
+++ README.md
@@ -0,0 +1,11 @@
+## Personal portfolio  
+
+## Ishlatilgan texnologiyalar:
+ - ## HTML 
+ - ## CSS 
+ - ## JavaScript 
+  
+## Maket dizayn ko'rinishi: ![design template](design.png)
+## Maketni yuklab olish uchun: [Yuklab olish uchun ustiga bosing](https://www.figma.com/file/Kr6JLLH8AOrkTuIGBviNdy/Personal-portfolio-Website-landing-page-UX-UI-kit-(Community)?node-id=6%3A4)
+
+## DEMO: [https://personal-portfolio-design.netlify.app/](https://personal-portfolio-design.netlify.app/)
---
+++ js/main.js
@@ -0,0 +1,56 @@
+let list = document.querySelectorAll(".list");
+let portfolioItem = document.querySelectorAll(".portfolio-item");
+
+for (let i = 0; i < list.length; i++) {
+  list[i].addEventListener("click", function () {
+    for (let j = 0; j < list.length; j++) {
+      list[j].classList.remove("active");
+    }
+    this.classList.add("active");
+
+    let dataFilter = this.getAttribute("data-filter");
+
+    for (let k = 0; k <= portfolioItem.length; k++) {
+      portfolioItem[k].classList.remove("active");
+      portfolioItem[k].classList.add("hide");
+      if (
+        portfolioItem[k].getAttribute("data-item") == dataFilter ||
+        dataFilter == "all"
+      ) {
+        portfolioItem[k].classList.remove("hide");
+        portfolioItem[k].classList.add("active");
+      }
+    }
+  });
+}
+
+const counters = document.querySelectorAll(".counter");
+const speed = 5;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = Number(counter.getAttribute("data-target"));
+    const count = Number(counter.innerText);
+    const inc = target / speed;
+    if (count < target) {
+      counter.innerText = count + inc;
+      setTimeout(updateCount, 250);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCount();
+});
+
+
+
+ var swiper = new Swiper(".mySwiper", {
+   slidesPerView: 3,
+   spaceBetween: 30,
+   freeMode: true,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+ });
